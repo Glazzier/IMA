d03c5191ed2cde03c6258e3285599801867f9cae
Glazzier <adrialopezfernandez@gmail.com>
2024-06-17 20:01:12 +0200

    Mini Update
    Ahora cuando se envia un formulario se menciona a Glazzier

--- scripts.js
+++ scripts.js
@@ -13,7 +13,12 @@ document.addEventListener("DOMContentLoaded", function () {
       const webhookURL =
         "https://discord.com/api/webhooks/1252318119356272691/48gYTey2uc1w-cDrwd1i_ClGgJ7sNAqwSGWjtMQGxt1DuvgdlC6dNqPLqg9u6v1h0Tvo";
 
-      sendEmbedToWebhook(formData, webhookURL, "Aplicación para Moderador");
+      sendEmbedToWebhook(
+        formData,
+        webhookURL,
+        "Aplicación para Moderador",
+        "<@631907198930386950>"
+      );
     });
   }
 
@@ -25,13 +30,19 @@ document.addEventListener("DOMContentLoaded", function () {
       const webhookURL =
         "https://discord.com/api/webhooks/1252313374507925624/5iY4n5CWTy9Kl6m9tIcL46jejBS3NtoqmGK1zqxeF5dqZy6ng984jSXAV21o6R4ThabP";
 
-      sendEmbedToWebhook(formData, webhookURL, "Respuestas del Examen GD");
+      sendEmbedToWebhook(
+        formData,
+        webhookURL,
+        "Respuestas del Examen GD",
+        "<@631907198930386950>"
+      );
     });
   }
 
-  function sendEmbedToWebhook(formData, webhookURL, title) {
+  function sendEmbedToWebhook(formData, webhookURL, title, mention) {
     const embed = {
       title: title,
+      description: `${mention}, aquí tienes la información:`,
       fields: [],
     };
 
@@ -51,6 +62,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
+        content: mention,
         embeds: [embed],
       }),
     })
